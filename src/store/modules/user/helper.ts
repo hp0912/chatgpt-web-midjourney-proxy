e126a1df3367a25e5a8486db237893c8dd287900
hp0912 <809211365@qq.com>
2024-09-30 14:30:45 +0800

    feat: 默认配置

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -18,9 +18,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Dooy/chatgpt-web-midjourney-proxy/main/src/assets/avatar.jpg',
+      avatar: 'https://img.houhoukang.com/uranus/ChatGPT.jpg',
       name:  t('mjset.sysname'),//'AI绘图',
-      description: 'Star on <a href="https://github.com/Dooy/chatgpt-web-midjourney-proxy" class="text-blue-500" target="_blank" >GitHub</a>',
+      description: 'Star on <a href="https://github.com/hp0912/chatgpt-web-midjourney-proxy" class="text-blue-500" target="_blank" >GitHub</a> -> <a href="https://github.com/Dooy/chatgpt-web-midjourney-proxy" class="text-blue-500" target="_blank" >Fork</a>',
     },
   }
 }
